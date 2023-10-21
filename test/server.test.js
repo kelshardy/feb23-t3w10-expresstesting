90f71ac477e2df0f666b2dae88c71d5190111332
kelshardy <14024@coderacademy.edu.au>
2023-10-21 11:20:12 +1100

    feat: testing, post request and data implemented

---
+++ test/server.test.js
@@ -0,0 +1,35 @@
+// Test the routes from server.js 
+
+const {app} = require('../src/server');
+
+// Import supertest so we can manage the app/server in tests properly
+const request = require('supertest');
+
+describe("Server root route exists and returns hello world", () => {
+	test("Root route exists and returns status 200", async () => {
+		const responseResult = await request(app).get("/");
+		expect(responseResult.statusCode).toEqual(200);
+	});
+
+	test("Root route exists and returns hello world message", async () => {
+		const response = await request(app).get("/");
+		expect(response.body.message).toEqual("Hello world!");
+	});
+});
+
+describe("POST to root route copies message in request body", () => {
+	test('POST request.body.message of "Hello world" returns received value of "Hello world"', async () => {
+		
+		let messageToSend = "Hello world";
+
+		const response = await request(app)
+		.post("/")
+		.send({
+			message: messageToSend
+		});
+
+		expect(response.body.received).toEqual(messageToSend);
+
+
+	});
+});
